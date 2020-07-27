6e2ce316592e0fd4369b70c31a0779d62bb9720e
Bluebird <stephany1445@hotmail.com>
2020-07-26 17:51:22 -0700

    Linked up my style sheet and javascript files to my html added a basic pseudocode to my js file

---
+++ Assets/java.js
@@ -0,0 +1,8 @@
+// Create a button
+//button will be called "START"
+//A timer will start
+//A question populates
+//Four options of correct answers display
+//if answered incorrectly, time is subtracted and new questions displays
+//if all questions are answered or time reaches zero the game is over
+//create a text box to type initials
